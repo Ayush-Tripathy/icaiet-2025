7d5709e450888335a6aef67e305f126c5bc8b617
ayushtripathy <63307858+Ayush-Tripathy@users.noreply.github.com>
2024-11-27 21:13:49 +0530

    temp: landing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,39 +7,43 @@ const Home = () => {
       <Navbar />
       <div className="relative">
         <div className="absolute inset-0 bg-[rgb(0,0,0/50)] h-screen"></div>
-        <div className="bg-[url(/XIM_BG1.jpg)] relative h-screen bg-no-repeat bg-cover text-white px-20 py-32">
-          <div className="">
-            <span className="font-[SatoshiBold] text-[3rem] uppercase leading-[0.9] text-outline">
-              International Conference on
-            </span>
-            <br />
-            <span className="font-[SatoshiBold] text-[4.5rem] uppercase leading-tight text-white">
-              Artificial Intelligence And
-            </span>
-            <br />
-            <span className="font-[SatoshiBold] text-[4.5rem] uppercase leading-tight text-white">
-              Emerging Technologies
-            </span>
-            <br />
-            <div className="flex flex-row items-center space-x-10">
-              <span className="font-[SatoshiBold] text-[4.5rem] uppercase leading-tight text-white">
-                (ICAIET) - 2025
+        <div className="bg-[url(/bg2.webp)] relative h-screen bg-no-repeat bg-cover text-white px-20 py-32">
+          <div className="flex flex-col gap-5">
+            <div>
+              <span className="font-[SatoshiBold] text-[3rem] uppercase leading-[0.9] text-outline">
+                International Conference on
               </span>
-              <a
-                href="test"
-                className="flex flex-row items-center gap-3 px-10 py-5 bg-[#ffffff3d] backdrop-blur-[1.25rem] text-lg uppercase rounded-full border border-[#fff]"
-              >
-                Register Now <ArrowRight size={24} />
-              </a>
+              <br />
+              <span className="font-[SatoshiBold] text-[4rem] uppercase leading-tight text-white">
+                Artificial Intelligence And
+              </span>
+              <br />
+              <span className="font-[SatoshiBold] text-[4rem] uppercase leading-tight text-white">
+                Emerging Technologies
+              </span>
+              <br />
+              <div className="flex flex-row items-center space-x-10">
+                <span className="font-[SatoshiBold] text-[4rem] uppercase leading-tight text-white">
+                  (ICAIET) - 2025
+                </span>
+                <a
+                  href="test"
+                  className="flex flex-row items-center gap-3 px-10 py-5 bg-[#ffffff3d] backdrop-blur-[1.25rem] text-lg uppercase rounded-full border border-[#fff]"
+                >
+                  Register Now <ArrowRight size={24} />
+                </a>
+              </div>
             </div>
 
-            <span className="font-[SatoshiBold] uppercase text-lg bg-[#ffffff26] py-3 px-6 rounded-lg backdrop-blur-[13px]">
-              August 28<sup>th</sup>–30<sup>th</sup>, 2025
-            </span>
-            <p className="font-[SatoshiBold] text-xl uppercase">
-              School of Computer Science and Engineering, XIM University
-              Bhubaneswar (New Campus), Harirajpur
-            </p>
+            <div className="flex flex-col items-start gap-3">
+              <span className="font-[SatoshiBold] uppercase text-lg bg-[#ffffff26] py-3 px-6 rounded-lg backdrop-blur-[13px]">
+                August 28<sup>th</sup>–30<sup>th</sup>, 2025
+              </span>
+              <p className="font-[SatoshiBold] text-xl uppercase">
+                School of Computer Science and Engineering, XIM University
+                Bhubaneswar (New Campus), Harirajpur
+              </p>
+            </div>
           </div>
         </div>
       </div>
