150176d8215cbeea09f0c9ccb2560fddc1c06792
ayushtripathy <63307858+Ayush-Tripathy@users.noreply.github.com>
2024-11-28 23:37:22 +0530

    -

--- src/lib/components/Footer.tsx
+++ src/lib/components/Footer.tsx
@@ -4,6 +4,7 @@ const Footer = () => {
       <div className="grid grid-cols-1 md:grid-cols-5 gap-10">
         <div className="col-span-2">
           <iframe
+            title="XIM University Location"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3745.4227708308176!2d85.75987517511702!3d20.15811538128217!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a19af103360ec8f%3A0x734b96dc8afdf0ef!2sXIM%20University!5e0!3m2!1sen!2sin!4v1732373764228!5m2!1sen!2sin"
             className="border border-input rounded-lg w-full h-full"
             allowFullScreen={false}
