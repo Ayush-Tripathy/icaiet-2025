c5a78fa6a6025bd625a494f0870aa4a97d7cc0f8
darknight08zz <prajapatiujjwal0802@gmail.com>
2026-05-08 16:42:24 +0530

    Fixing ack issue

--- src/pages/Acknowledgement.tsx
+++ src/pages/Acknowledgement.tsx
@@ -13,3 +13,19 @@ export default function Acknowledgement() {
     </div>
   );
 }
+{/*
+export const dynamic = 'force-static';  // 👈 Add this line
+
+export default function Acknowledgement() {
+  return (
+    <div className="min-h-screen py-8 pt-32 primary-padding font-[SatoshiRegular]">
+      <div className="container mx-auto px-4">
+        <p className="text-lg leading-relaxed text-gray-700">
+          The Microsoft CMT service was used for managing the peer-reviewing process for this conference. 
+          This service was provided for free by Microsoft and they bore all expenses, 
+          including costs for Azure cloud services as well as for software development and support.
+        </p>
+      </div>
+    </div>
+  );
+}*/}
